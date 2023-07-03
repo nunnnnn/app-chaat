a59bdc8a713fcbc07ca169b4d98455f56bdc9c75
Your Name <you@example.com>
2023-07-03 21:24:37 +0700

    be

--- b/controllers/postController.js
+++ b/controllers/postController.js
@@ -1,3 +1,68 @@
+// const Post = require("../models/post");
+
+// module.exports.createPost = async (req, res, next) => {
+//   try {
+//     const { image, title, branch, attach_link, detail, create_at, teacher_id } =
+//       req.body;
+//     await Post.create({
+//       image,
+//       title,
+//       branch,
+//       attach_link,
+//       detail,
+//       create_at,
+//       teacher_id,
+//     });
+//     return res.json({ msg: "create success!", status: true });
+//   } catch (ex) {
+//     next(ex);
+//   }
+// };
+// module.exports.getAllPost = async (req, res, next) => {
+//   try {
+//     const result = await Post.find();
+//     return res.json(result);
+//   } catch (ex) {
+//     next(ex);
+//   }
+// };
+
+// module.exports.getPostById = async (req, res, next) => {
+//   try {
+//     const result = await Post.findById({_id: req.params.eid});
+//     return res.json(result);
+//   } catch (ex) {
+//     next(ex);
+//   }
+// };
+
+// module.exports.updatePostById = async (req, res, next) => {
+//   try {
+//     const {  title, detail, attach_link } = req.body;
+//     await Post.updateOne(
+//       { _id: req.params.eid },
+//       {
+//         // image,
+//         title,
+//         detail,
+//         attach_link,
+//       }
+//     );
+//     return res.json({ status: 200, msg: "Update success!" });
+//   } catch (ex) {
+//     next(ex);
+//   }
+// };
+
+// module.exports.deletePostById = async (req, res, next) => {
+//   try {
+//     await Post.remove({ _id: req.params.eid });
+//     return res.json({ status: 200, msg: "Delete success!" });
+//   } catch (ex) {
+//     next(ex);
+//   }
+// };
+
 const Post = require("../models/post");
 
 module.exports.createPost = async (req, res, next) => {
@@ -38,13 +103,15 @@ module.exports.getPostById = async (req, res, next) => {
 
 module.exports.updatePostById = async (req, res, next) => {
   try {
-    const { title, detail, attach_link } = req.body;
+    const { image, title, branch, attach_link, detail } = req.body;
     await Post.updateOne(
       { _id: req.params.eid },
       {
+        image,
         title,
-        detail,
+        branch,
         attach_link,
+        detail,
       }
     );
     return res.json({ status: 200, msg: "Update success!" });
